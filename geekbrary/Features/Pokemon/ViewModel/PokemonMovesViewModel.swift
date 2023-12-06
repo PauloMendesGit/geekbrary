7b8fc829b6a5a47cdebfd2f8f6e86f21541df7ec
Paulo Mendes <paumen@signicat.com>
2023-12-06 17:12:09 +0000

    git ignore update

--- geekbrary/Features/Pokemon/ViewModel/PokemonMovesViewModel.swift
+++ geekbrary/Features/Pokemon/ViewModel/PokemonMovesViewModel.swift
@@ -28,7 +28,6 @@ class PokemonMovesViewModel: ObservableObject {
                     }
                 }
                 dispatchGroup.leave()
-                //   }
             }
         }
         
